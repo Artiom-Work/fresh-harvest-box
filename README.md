616ea82487b3396b489f2bca82926f6b03c25dca
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-02-25 10:33:58 +0300

    added promo

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
         Performance in desktop = 100%
         Performance in mobile = 100%
+
+2.  Added footer.
+
+        Performance in desktop = 100%
+        Performance in mobile = 100%
--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//lock body after the mobile menu appeared
+const menuSwitcher = document.getElementById('menu-switch');
+const mobileMenu = document.querySelector('.mobile-menu__wrapper');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+menuSwitcher.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		bodyLock();
+	} else if (!e.target.checked) {
+		bodyUnlock();
+	}
+});
+mobileMenu.addEventListener('click', (e) => {
+	menuSwitcher.checked = false;
+	bodyUnlock();
+});
+
+function bodyLock() {
+	document.body.classList.add('lock-body');
+	if (main && footer) {
+		main.classList.add('hide-layer');
+		footer.classList.add('hide-layer');
+	}
+}
+function bodyUnlock() {
+	document.body.classList.remove('lock-body');
+	if (main && footer) {
+		function visibleLayer() {
+			main.classList.remove('hide-layer');
+			footer.classList.remove('hide-layer');
+		}
+
+		setTimeout(visibleLayer, 200);
+	}
+}
