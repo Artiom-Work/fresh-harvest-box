7da73a05a1418a1305b49f90e736dfaf33cea8aa
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-02-27 01:28:26 +0300

    added section how-it-works

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 
         Performance in desktop = 100%
         Performance in mobile = 100%
+
+3.  Added promo.
+
+        Performance in desktop = 98%
+        Performance in mobile = 100%
+        		2% optimization stole js file
--- js/main.js
+++ js/main.js
@@ -1,38 +1,45 @@
-// 'use strict';
+'use strict';
+const mediaQueryMobile = window.matchMedia('(max-width: 767px)');
+//lock body after the mobile menu appeared
+const menuSwitcher = document.getElementById('menu-switch');
+const mobileMenu = document.querySelector('.mobile-menu__wrapper');
+const linkToMobileMenu = document.querySelector('.link-to-mobile-menu');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
-// //lock body after the mobile menu appeared
-// const menuSwitcher = document.getElementById('menu-switch');
-// const mobileMenu = document.querySelector('.mobile-menu__wrapper');
-// const main = document.querySelector('main');
-// const footer = document.querySelector('footer');
+menuSwitcher.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		bodyLock();
+	} else if (!e.target.checked) {
+		bodyUnlock();
+	}
+});
+linkToMobileMenu.addEventListener('click', (e) => {
+	if (mediaQueryMobile.matches) {
+		menuSwitcher.checked = true;
+		bodyLock();
+	}
+});
+mobileMenu.addEventListener('click', (e) => {
+	menuSwitcher.checked = false;
+	bodyUnlock();
+});
 
-// menuSwitcher.addEventListener('change', (e) => {
-// 	if (e.target.checked) {
-// 		bodyLock();
-// 	} else if (!e.target.checked) {
-// 		bodyUnlock();
-// 	}
-// });
-// mobileMenu.addEventListener('click', (e) => {
-// 	menuSwitcher.checked = false;
-// 	bodyUnlock();
-// });
+function bodyLock() {
+	document.body.classList.add('lock-body');
+	if (main && footer) {
+		main.classList.add('hide-layer');
+		footer.classList.add('hide-layer');
+	}
+}
+function bodyUnlock() {
+	document.body.classList.remove('lock-body');
+	if (main && footer) {
+		function visibleLayer() {
+			main.classList.remove('hide-layer');
+			footer.classList.remove('hide-layer');
+		}
 
-// function bodyLock() {
-// 	document.body.classList.add('lock-body');
-// 	if (main && footer) {
-// 		main.classList.add('hide-layer');
-// 		footer.classList.add('hide-layer');
-// 	}
-// }
-// function bodyUnlock() {
-// 	document.body.classList.remove('lock-body');
-// 	if (main && footer) {
-// 		function visibleLayer() {
-// 			main.classList.remove('hide-layer');
-// 			footer.classList.remove('hide-layer');
-// 		}
-
-// 		setTimeout(visibleLayer, 200);
-// 	}
-// }
+		setTimeout(visibleLayer, 200);
+	}
+}
